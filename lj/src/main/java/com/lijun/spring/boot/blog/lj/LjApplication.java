f4c869e35e255c445a385a8559441937f7683597
lijun <lj598950610@163.com>
2020-06-05 16:42:42 +0800

    初初始化

---
+++ lj/src/main/java/com/lijun/spring/boot/blog/lj/LjApplication.java
@@ -0,0 +1,13 @@
+package com.lijun.spring.boot.blog.lj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LjApplication.class, args);
+	}
+
+}
---
+++ lj/src/test/java/com/lijun/spring/boot/blog/lj/LjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lijun.spring.boot.blog.lj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
